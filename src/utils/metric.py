d43711a32be7402aebf1d2859256744d15212839
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2023-09-26 18:04:02 +0800

    add config "primary_criterion"  to choose best checkpoint

--- src/utils/metric.py
+++ src/utils/metric.py
@@ -2,16 +2,37 @@ from torch import nn
 
 
 class MetricBase:
-    def get_metrics(self, preds, gts):
+    def __init__(self, cfg):
+        self.primary_criterion = cfg.model.primary_criterion
+    
+    def get_metrics(self, cfg):
         raise NotImplementedError
 
     def choose_best(self, metric: dict, best_metric: dict):
-        raise NotImplementedError
+        def compare_loss(m, b_m):
+            if m['loss'] < b_m['loss']:
+                return m, True
+            else:
+                return b_m, False
+            
+        def compare_primary_criterion(m, b_m):
+            assert self.primary_criterion in m, f'best_criterion "{self.primary_criterion}" not in metric {m}'
+            if m[self.primary_criterion] < b_m[self.primary_criterion]:
+                return m, True
+            else:
+                return b_m, False
+
+        if metric == {} or best_metric == {}:
+            return metric, True
+        elif self.primary_criterion is not None:
+            return compare_primary_criterion(metric, best_metric)
+        else:
+            return compare_loss(metric, best_metric)
         
 
 class SimpleMetric(MetricBase):
     def __init__(self, cfg):
-        self.cfg = cfg
+        super().__init__(cfg)
         self.l1_loss = nn.L1Loss()
     
     def get_metrics(self, preds, gts):
@@ -19,15 +40,3 @@ class SimpleMetric(MetricBase):
         return {
             'L1_loss': l1_loss
             }, None
-            
-    def choose_best(self, metric: dict, best_metric: dict):
-        def compare_loss(m, b_m):
-            if m['loss'] < b_m['loss']:
-                return m, True
-            else:
-                return b_m, False
-
-        if metric == {} or best_metric == {}:
-            return metric, True
-        else:
-            return compare_loss(metric, best_metric)
