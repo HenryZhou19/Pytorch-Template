9d4e9501c46191a4078ef25eaca43c758053abf2
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2023-10-24 12:56:14 +0800

    fix one bug in basic_layers.py

--- src/models/modules/basic_layers.py
+++ src/models/modules/basic_layers.py
@@ -9,10 +9,10 @@ class MLP(nn.Module):
         super().__init__()
         self.mlp = nn.Sequential()
         for idx, out_channel in enumerate(out_channels):
-            self.layers.append(nn.Linear(in_channel, out_channel))
+            self.mlp.append(nn.Linear(in_channel, out_channel))
             if idx < len(out_channels) - 1:
-                self.layers.append(activate_layer())
-            self.layers.append(nn.Dropout(drop))
+                self.mlp.append(activate_layer())
+            self.mlp.append(nn.Dropout(drop))
             in_channel = out_channel
         self.out_channel = out_channels[-1]
 
