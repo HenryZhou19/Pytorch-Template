8fcce5c38d496f62d1967a2ac7a5b4405851df6f
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2023-11-16 23:46:36 +0800

    add TimeDownscaleUNet3d

--- src/models/modules/unet.py
+++ src/models/modules/unet.py
@@ -50,15 +50,20 @@ class DownSampling(nn.Module):
 
 
 class UpSampling(nn.Module):
-    def __init__(self, in_channels, cat_channels, out_channels, dimension, use_conv_transpose, padding_mode):
+    def __init__(self, in_channels, cat_channels, out_channels, dimension, use_conv_transpose, padding_mode, no_up_dim=None):
         super().__init__()
         assert dimension in [2, 3], 'Unsupported dimension'
         ConvTransposeXd = nn.ConvTranspose2d if dimension == 2 else nn.ConvTranspose3d
-        
+        kernel_size = [2] * dimension
+        if no_up_dim is not None:
+            if isinstance(no_up_dim, int):
+                no_up_dim = (no_up_dim, )
+            for d in no_up_dim:
+                kernel_size[d - 2] = 1
         if use_conv_transpose:
-            self.up = ConvTransposeXd(in_channels, in_channels, kernel_size=2, stride=2)
+            self.up = ConvTransposeXd(in_channels, in_channels, kernel_size=kernel_size, stride=kernel_size)
         else:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear' if dimension == 2 else 'trilinear', align_corners=True)
+            self.up = nn.Upsample(scale_factor=kernel_size, mode='bilinear' if dimension == 2 else 'trilinear', align_corners=True)
         self.conv = ConvBlock(in_channels + cat_channels, out_channels, dimension, padding_mode=padding_mode)
 
     def forward(self, x, cat_features):
@@ -146,6 +151,48 @@ class TimeUpscaleUNet3d(nn.Module):
             if idx < len(self.down_layers) - 1:  # [N, c2, L_fused, h/2, w/2]
                 cat_list.append(self.special_up[idx + 1](x))
 
+        # up
+        for up_layer in self.up_layers:
+            x = up_layer(x, cat_list.pop())
+        x = self.out_conv(x)
+        
+        return x
+    
+
+class TimeDownscaleUNet3d(nn.Module):
+    def __init__(self, in_channels, down_scale=4, layer_out_channels=[64, 128, 256], final_out_channels=None, use_conv_transpose=False, padding_mode='replicate'):
+        super().__init__()
+        assert 2 ** (len(layer_out_channels) - 1) == down_scale, 'down_scale must be 2 ** (len(layer_out_channels) - 1)'
+        kernel_size = (2, 1, 1)
+        stride = kernel_size
+        
+        if final_out_channels is None:
+            final_out_channels = in_channels
+
+        self.in_conv = ConvBlock(in_channels, layer_out_channels[0], 3, padding_mode=padding_mode)
+        self.down_layers = nn.ModuleList([
+            DownSampling(layer_out_channels[i], layer_out_channels[i + 1], 3, padding_mode) for i in range(len(layer_out_channels) - 1)
+        ])
+        self.special_down = nn.ModuleList([
+            nn.MaxPool3d(
+                kernel_size=tuple((torch.tensor(kernel_size) ** (len(layer_out_channels) - 1 - i)).tolist()),
+                stride=tuple((torch.tensor(stride) ** (len(layer_out_channels) - 1 - i)).tolist())) for i in range(len(layer_out_channels) - 1)
+        ])
+        self.up_layers = nn.ModuleList([
+            UpSampling(layer_out_channels[i], layer_out_channels[i - 1], layer_out_channels[i - 1], 3, use_conv_transpose, padding_mode, no_up_dim=2) for i in range(len(layer_out_channels) - 1, 0, -1)
+        ])
+        self.out_conv = LastConv(layer_out_channels[0], final_out_channels, 3)
+
+    def forward(self, x):  # [N, 3, L_fused, h, w] -> [N, 3, L_all, h ,w]
+        # down
+        cat_list = []
+        x = self.in_conv(x)  # [N, c1, L_fused, h, w]
+        cat_list.append(self.special_down[0](x))
+        for idx, down_layer in enumerate(self.down_layers):
+            x = down_layer(x)
+            if idx < len(self.down_layers) - 1:  # [N, c2, L_fused, h/2, w/2]
+                cat_list.append(self.special_down[idx + 1](x))
+                
         # up
         for up_layer in self.up_layers:
             x = up_layer(x, cat_list.pop())
