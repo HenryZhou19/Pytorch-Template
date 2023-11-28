d02b0e80e3d6fd9343e11f6172a8351c3237fd5c
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2023-11-28 19:16:39 +0800

    simplify "all_gather" in misc

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -439,49 +439,16 @@ class DistMisc:
             time.sleep(DistMisc.get_rank() * 0.1)
     
     @staticmethod
-    def all_gather(data):
+    def all_gather(x: torch.Tensor):
 
-        """
-        Run all_gather on arbitrary picklable data (not necessarily tensors)
-        Args:
-            data: any picklable object
-        Returns:
-            list[data]: list of data gathered from each rank
-        """
         world_size = DistMisc.get_world_size()
         if world_size == 1:
-            return [data]
+            return [x]
+            
+        x_list = [torch.empty_like(x)] * world_size
+        dist.all_gather(x_list, x)
 
-        # serialized to a Tensor
-        buffer = pickle.dumps(data)
-        storage = torch.ByteStorage.from_buffer(buffer)
-        tensor = torch.ByteTensor(storage).to('cuda')
-
-        # obtain Tensor size of each rank
-        local_size = torch.as_tensor([tensor.numel()], device='cuda')
-        size_list = [torch.as_tensor([0], device='cuda') for _ in range(world_size)]
-        dist.all_gather(size_list, local_size)
-        size_list = [int(size.item()) for size in size_list]
-        max_size = max(size_list)
-
-        # receiving Tensor from all ranks
-        # we pad the tensor because torch all_gather does not support
-        # gathering tensors of different shapes
-        tensor_list = [None] * world_size
-        # tensor_list = []
-        # for _ in size_list:
-        #     tensor_list.append(torch.empty((max_size,), dtype=torch.uint8, device='cuda'))
-        if local_size != max_size:
-            padding = torch.empty(size=(max_size - local_size,), dtype=torch.uint8, device='cuda')
-            tensor = torch.cat((tensor, padding), dim=0)
-        dist.all_gather(tensor_list, tensor)
-
-        data_list = []
-        for size, tensor in zip(size_list, tensor_list):
-            buffer = tensor.cpu().numpy().tobytes()[:size]
-            data_list.append(pickle.loads(buffer))
-
-        return data_list
+        return x_list
 
     @staticmethod
     def reduce_dict(input_dict, average=True):
