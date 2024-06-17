670cd8faf8f0bb00217924700785faaebba46a04
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2024-06-17 19:52:42 +0800

    better PatchEmbedding2,3D

--- src/models/modules/basic_layers.py
+++ src/models/modules/basic_layers.py
@@ -16,7 +16,7 @@ class MLP(nn.Module):
                 self.mlp.append(nn.Dropout(dropout))
             in_channel = out_channel
         self.out_channel = out_channels[-1]
-
+        
     def forward(self, x):
         x = self.mlp(x)
         return x
@@ -37,14 +37,14 @@ class PositionalEncoding(nn.Module):
             self.pe = nn.Parameter(torch.zeros(1, max_seq_length, d_model))
         
         self.dropout = nn.Dropout(p=dropout)
-
+        
     def forward(self, x):
         x = x + self.pe[:, :x.size(1), :]  # [N, L, d_model]  # [N, L, d_model]
         return self.dropout(x)
-    
-    
+
+
 class PatchEmbedding2D(nn.Module):
-    def __init__(self, tensor_hw=(224, 224), patch_size=(16, 16), stride=(16, 16), in_channels=3, embed_dim=768, norm_layer=nn.Identity):
+    def __init__(self, tensor_hw=(224, 224), patch_size=(16, 16), stride=(16, 16), in_channels=3, embed_dim=768, norm_layer=nn.Identity, flatten=True, check_input=False):
         super().__init__()
         self.tensor_hw = tensor_hw
         self.patch_size = patch_size
@@ -60,19 +60,25 @@ class PatchEmbedding2D(nn.Module):
         self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=stride)
         self.norm = norm_layer(embed_dim)
         
+        self.flatten = flatten
+        self.check_input = check_input
+        
     def forward(self, x: torch.Tensor):
-        N, C, H, W = x.shape
-        assert H == self.tensor_hw[0] and W == self.tensor_hw[1], f'Predefined 2D-image size {self.tensor_hw} does not match input 2D-image size {x.shape[2:]}'
-        assert C == self.in_channels, f'Input channel {C} does not match predefined channel {self.in_channels}'
+        if self.check_input:
+            N, C, H, W = x.shape
+            assert H == self.tensor_hw[0] and W == self.tensor_hw[1], f'Predefined 2D-image size {self.tensor_hw} does not match input 2D-image size {x.shape[2:]}'
+            assert C == self.in_channels, f'Input channel {C} does not match predefined channel {self.in_channels}'
         x = self.proj(x)  # [N, C, H, W] -> [N, embed_dim, grid_size[0], grid_size[1]]
         
-        x = x.flatten(2).transpose(1, 2)  # [N, embed_dim, grid_size[0], grid_size[1]] -> [N, num_patches, embed_dim]
+        x = x.permute(0, 2, 3, 1)  # [N, grid_size[0], grid_size[1], embed_dim]
+        if self.flatten:
+            x = x.flatten(1, -2)  # [N, grid_size[0], grid_size[1], embed_dim] -> [N, num_patches, embed_dim]
         x = self.norm(x)
         return x
 
 
 class PatchEmbedding3D(nn.Module):
-    def __init__(self, tensor_dhw=(32, 224, 224), patch_size=(2, 16, 16), stride=(2, 16, 16), in_channels=3, embed_dim=768, norm_layer=nn.Identity):
+    def __init__(self, tensor_dhw=(32, 224, 224), patch_size=(2, 16, 16), stride=(2, 16, 16), in_channels=3, embed_dim=768, norm_layer=nn.Identity, flatten=True, check_input=False):
         super().__init__()
         self.tensor_dhw = tensor_dhw
         self.patch_size = patch_size
@@ -89,12 +95,18 @@ class PatchEmbedding3D(nn.Module):
         self.proj = nn.Conv3d(in_channels, embed_dim, kernel_size=patch_size, stride=stride)
         self.norm = norm_layer(embed_dim)
         
+        self.flatten = flatten
+        self.check_input = check_input
+        
     def forward(self, x: torch.Tensor):
-        N, C, D, H, W = x.shape
-        assert D == self.tensor_dhw[0] and H == self.tensor_dhw[1] and W == self.tensor_dhw[2], f'Predefined 3D-image size {self.tensor_dhw} does not match input 3D-image size {x.shape[2:]}'
-        assert C == self.in_channels, f'Input channel {C} does not match predefined channel {self.in_channels}'
+        if self.check_input:
+            N, C, D, H, W = x.shape
+            assert D == self.tensor_dhw[0] and H == self.tensor_dhw[1] and W == self.tensor_dhw[2], f'Predefined 3D-image size {self.tensor_dhw} does not match input 3D-image size {x.shape[2:]}'
+            assert C == self.in_channels, f'Input channel {C} does not match predefined channel {self.in_channels}'
         x = self.proj(x)  # [N, C, D, H, W] -> [N, embed_dim, grid_size[0], grid_size[1], grid_size[2]]
         
-        x = x.flatten(2).transpose(1, 2)  # [N, embed_dim, grid_size[0], grid_size[1], grid_size[2]] -> [N, num_patches, embed_dim]
+        x = x.permute(0, 2, 3, 4, 1)  # [N, grid_size[0], grid_size[1], grid_size[2], embed_dim]
+        if self.flatten:
+            x = x.flatten(1, -2)  # [N, grid_size[0], grid_size[1], grid_size[2], embed_dim] -> [N, num_patches, embed_dim]
         x = self.norm(x)
         return x
