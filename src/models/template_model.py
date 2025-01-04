f379eb2ace1bf178166561df6ff683bce5c14ae9
Jiaheng Zhou <zhoujiaheng2022@ia.ac.cn>
2025-01-04 09:53:30 +0800

    fix one bug in template_models

--- src/models/template_model.py
+++ src/models/template_model.py
@@ -104,25 +104,72 @@ class LeNetMultiOptimizer(ModelBase):
     def __init__(self, cfg):
         super().__init__(cfg)
         assert cfg.model.backbone == 'default'
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        fc1 = nn.Linear(16 * 4 * 4, 120)
+        fc2 = nn.Linear(120, 84)
+        fc3 = nn.Linear(84, 10)
         
-        self.convs = nn.ModuleList([self.conv1, self.conv2])
-        self.fcs = nn.ModuleList([self.fc1, self.fc2, self.fc3])
+        self.convs = nn.ModuleList([conv1, conv2])
+        self.fcs = nn.ModuleList([fc1, fc2, fc3])
 
     def forward(self, inputs: dict) -> dict:
         x = inputs['x']
+        x = F.relu(self.convs[0](x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.convs[1](x))
+        x_conv_out = F.max_pool2d(x, 2)
+        x = x_conv_out.view(-1, 16 * 4 * 4).detach()
+        x = F.relu(self.fcs[0](x))
+        x = F.relu(self.fcs[1](x))
+        x = self.fcs[2](x)
+        return {
+            'conv_out': x_conv_out,
+            'pred_scores': x
+        }
+        
+
+class LeNetConvs(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+
+    def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
-        x_conv_out = F.max_pool2d(x, 2)
-        x = x_conv_out.view(-1, 16 * 4 * 4).detach()
+        x = F.max_pool2d(x, 2)
+        return x
+    
+    
+class LeNetFCs(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        return x
+        
+
+@model_register('lenet_multi_optimizer_v2')
+class LeNetMultiOptimizerV2(ModelBase):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        assert cfg.model.backbone == 'default'       
+        self.convs = LeNetConvs()
+        self.fcs = LeNetFCs()
+
+    def forward(self, inputs: dict) -> dict:
+        x = inputs['x']
+        x_conv_out = self.convs(x)
+        x = x_conv_out.view(-1, 16 * 4 * 4).detach()
+        x = self.fcs(x)
         return {
             'conv_out': x_conv_out,
             'pred_scores': x
